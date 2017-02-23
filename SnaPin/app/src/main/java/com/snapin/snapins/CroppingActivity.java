60120487762340937356e69bbdb8d9ee41641093
Prasanna Kumar <prasanna.kumar@mobinius.com>
2017-02-23 23:50:47 +0530

    Added keerthi code

---
+++ SnaPin/app/src/main/java/com/snapin/snapins/CroppingActivity.java
@@ -0,0 +1,106 @@
+package com.snapin.snapins;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class CroppingActivity extends AppCompatActivity {
+    ImageView preCroppingImageView, postCroppingImageView;
+    Intent CropIntent;
+    File file;
+    Uri uri;
+    Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cropping);
+        preCroppingImageView =(ImageView) findViewById(R.id.first_image_view);
+        postCroppingImageView =(ImageView)findViewById(R.id.second_image_view);
+
+        Bundle extras = getIntent().getExtras();
+        byte[] b = extras.getByteArray("picture");
+
+        bitmap = BitmapFactory.decodeByteArray(b, 0, b.length);
+        try{
+             file = new File(Environment.getExternalStorageDirectory(),
+                    "file" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+        }catch (Exception e){
+
+        }
+
+        OutputStream _out = null;
+        try {
+            _out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, _out);
+            _out.close();
+        }catch (Exception e){
+
+        }
+
+        uri = Uri.fromFile(file);
+
+        preCroppingImageView.bringToFront();
+        preCroppingImageView.setImageBitmap(bitmap);
+        ImageCropFunction();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+
+            if (data != null) {
+                Bundle bundle = data.getExtras();
+                bitmap = bundle.getParcelable("data");
+                postCroppingImageView.bringToFront();
+                preCroppingImageView.setImageBitmap(null);
+                postCroppingImageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    public void CancelMethod(View view){
+        preCroppingImageView.bringToFront();
+        postCroppingImageView.setImageBitmap(null);
+        ImageCropFunction();
+
+    }
+
+    public void SaveMethod(View view){
+        Toast.makeText(getApplicationContext(),"Need to Save",Toast.LENGTH_SHORT).show();
+    }
+
+    public void ImageCropFunction() {
+
+        // Image Crop Code
+        try {
+            CropIntent = new Intent("com.android.camera.action.CROP");
+            CropIntent.setDataAndType(uri, "image/*");
+            CropIntent.putExtra("crop", "true");
+           /* CropIntent.putExtra("outputX", 400);
+            CropIntent.putExtra("outputY", 100);
+            CropIntent.putExtra("aspectX", 3);
+            CropIntent.putExtra("aspectY", 1);
+            CropIntent.putExtra("scaleUpIfNeeded", true);*/
+            CropIntent.putExtra("return-data", true);
+            startActivityForResult(CropIntent, 1);
+
+        } catch (ActivityNotFoundException e) {
+        }
+    }
+    //Image Crop Code End Here
+}
---
+++ SnaPin/app/src/main/java/com/snapin/snapins/MainActivity.java
@@ -0,0 +1,145 @@
+package com.snapin.snapins;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    TouchImageView imageView;
+    Button buttonCamera, buttonGallery ;
+    Intent CamIntent, GalIntent ;
+    public  static final int RequestPermissionCode  = 1 ;
+    static Bitmap bitmap;
+    Button gotoCroppingActivityButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (TouchImageView) findViewById(R.id.imageview);
+        buttonCamera = (Button)findViewById(R.id.button2);
+        buttonGallery = (Button)findViewById(R.id.button1);
+        gotoCroppingActivityButton = (Button) findViewById(R.id.goto_crop_button);
+        gotoCroppingActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                imageView.setDrawingCacheEnabled(true);
+                Bitmap _b = imageView.getDrawingCache();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                _b.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                byte[] b = baos.toByteArray();
+
+                Intent intent = new Intent(getApplication(), CroppingActivity.class);
+                intent.putExtra("picture", b);
+                startActivity(intent);
+
+            }
+        });
+
+        EnableRuntimePermission();
+
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClickImageFromCamera() ;
+            }
+        });
+
+        buttonGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GetImageFromGallery();
+            }
+        });
+    }
+
+
+    public void ClickImageFromCamera() {
+
+        CamIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        CamIntent.putExtra("data", true);
+        startActivityForResult(CamIntent, 0);
+    }
+
+    public void GetImageFromGallery(){
+
+        GalIntent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(Intent.createChooser(GalIntent, "Select Image From Gallery"), 2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+
+             bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+
+        }
+        else if (requestCode == 2) {
+
+            if (data != null) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                // Get the cursor
+                Cursor cursor = getApplicationContext().getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                imageView.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
+
+            }
+        }
+    }
+
+    public void EnableRuntimePermission(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                Manifest.permission.CAMERA))
+        {
+            Toast.makeText(MainActivity.this,"CAMERA permission allows us to Access CAMERA app", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{
+                    Manifest.permission.CAMERA}, RequestPermissionCode);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int RC, String per[], int[] PResult) {
+
+        switch (RC) {
+
+            case RequestPermissionCode:
+
+                if (PResult.length > 0 && PResult[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this,
+                            "Permission Granted, Now your application can access CAMERA.",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this,
+                            "Permission Canceled, Now your application cannot access CAMERA.",
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+}
